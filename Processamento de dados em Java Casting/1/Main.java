9479efa8c841452cca0e39700e6b419a534ced74
Rodrigo Jose Fagundes <rodrigofgds@hotmail.com>
2023-03-04 16:49:58 -0300

    Processamento de dados em Java Casting
    Aula 25.

---
+++ Processamento de dados em Java Casting/1/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+public static void main(String[] args){
+
+int x, y;
+double z = 20;
+x = 5;
+y = 2 * x;
+
+
+System.out.println(x);
+System.out.println(y);
+System.out.println(z);
+
+}
+
+}
