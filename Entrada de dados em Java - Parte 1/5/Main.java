3eb1a867f03cec6ad5b5d1fa15473d90a2d6405d
Rodrigo Jose Fagundes <rodrigofgds@hotmail.com>
2023-03-04 16:52:23 -0300

    Entrada de dados em Java - Parte 1
    Aula 26.

---
+++ Entrada de dados em Java - Parte 1/5/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+public static void main (String[] args) {
+
+Scanner sc = new Scanner(System.in);
+
+String x;
+int y;
+double z;
+
+x = sc.next();
+y = sc.nextInt();
+z = sc.nextDouble();
+
+System.out.println("dados digitados ");
+System.out.println(x);
+System.out.println(y);
+System.out.println(z);
+
+
+sc.close();
+
+}
+
+}
