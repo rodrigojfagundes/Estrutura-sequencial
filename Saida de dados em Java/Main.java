7d5c3e05771ac4e39a0e4cea1138da5ad78894ff
Rodrigo Jose Fagundes <rodrigofgds@hotmail.com>
2023-03-04 16:45:36 -0300

    Saida de dados em Java

---
+++ Saida de dados em Java/Main.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+
+public class Main {
+
+public static void main(String[] args) {
+
+int y = 32;
+double x = 10.35784;
+String nome = "Maria";
+int idade = 31;
+double renda = 4000.0;
+
+System.out.println(y);
+System.out.println(x);
+Locale.setDefault(Locale.US);
+System.out.printf("%.2f%n", x);
+System.out.printf("%.2f%n", x);
+System.out.println("bom dia!");
+
+System.out.print("RESULTADO =" + x + " METROS");
+
+System.out.printf("RESULTADO = %.2f metros%n", x);
+
+System.out.printf("%s tem %d anos e ganha R$ %,2f reais%n", nome, idade, renda);
+
+}
+
+}
